362106ae2d9f0515e93546d4852c32823bd16bc8
shadi <shadi@thisisdevelopment.nl>
2023-06-12 14:35:39 +0200

    add language switcher

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,11 @@
 export type Locale = 'en' | 'nl';
 
+interface LanguageType {
+  code: string;
+  label: string;
+  locale: Locale;
+}
+
 export const fallbackLng = 'en';
 export const locales: Locale[] = [fallbackLng, 'nl'];
 export const defaultNS = 'translation';
@@ -20,3 +26,12 @@ export function getOptions(lng: Locale = fallbackLng, ns: string | string[] = de
     ns
   };
 }
+
+export const languages: readonly LanguageType[] = [
+  { code: 'NL', label: 'Dutch', locale: 'nl' },
+  {
+    code: 'US',
+    label: 'English',
+    locale: 'en'
+  }
+];
